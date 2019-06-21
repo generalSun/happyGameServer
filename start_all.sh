4014961c6772f2064ca4a0ff5933048999b3e4c5
generalSun <542981526@qq.com>
2019-06-21 15:02:30 +0800

    init

--- start_all.sh
+++ start_all.sh
@@ -1,3 +1,7 @@
-nohup node ./account_server/app.js ../configs.js &
-nohup node ./hall_server/app.js ../configs.js &
-nohup node ./game_server/app.js ../configs.js &
+# nohup node ./account_server/app.js ../configs.js &
+# nohup node ./hall_server/app.js ../configs.js &
+# nohup node ./game_server/app.js ../configs.js &
+
+node ./account_server/app.js ../configs.js
+node ./hall_server/app.js ../configs.js
+node ./game_server/app.js ../configs.js
